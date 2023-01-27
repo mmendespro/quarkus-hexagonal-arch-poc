6446cf1f389cfe722227d85dabba8cc98e79827d
mmendespro <marcelo.mendes.gomes@gmail.com>
2023-01-27 10:04:49 -0300

    code improvements

--- src/main/java/net/local/demo/hexagonal/application/domain/entities/Account.java
+++ src/main/java/net/local/demo/hexagonal/application/domain/entities/Account.java
@@ -1,7 +1,6 @@
 package net.local.demo.hexagonal.application.domain.entities;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import net.local.demo.hexagonal.application.domain.valueObjects.Money;
